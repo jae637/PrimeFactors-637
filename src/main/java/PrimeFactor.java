21e62103e99c634dff50c8b3c5c099de651b2ca4
cjh <kfc@email.com>
2025-06-04 10:44:44 +0900

    [refector] 41p 코드 정규화

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -6,11 +6,9 @@ public class PrimeFactor {
         List<Integer> result = new ArrayList<>();
         if(number>1){
             if(number ==4 ){
-                if(number%2 ==0){
+                while(number%2==0){
                     result.add(2);
-                }
-                if(number%2 ==0){
-                    result.add(2);
+                    number/=2;
                 }
             }else{
                 result.add(number);
