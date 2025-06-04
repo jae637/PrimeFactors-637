53abe238cc1a4172a49a5ad0b33bcaa0002f71dd
cjh <kfc@email.com>
2025-06-04 10:55:49 +0900

    [refactor] 54p 사용되지 않는 조건분기 삭제

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,15 +4,11 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> result = new ArrayList<>();
-        if(number>=1){
-            for (int divider =2 ;number >1;divider++){
-                while(number%divider==0){
-                    result.add(divider);
-                    number/=divider;
-                }
+        for (int divider =2 ;number >1;divider++){
+            while(number%divider==0){
+                result.add(divider);
+                number/=divider;
             }
-        } else{
-            result.add(number);
         }
         return result;
     }
