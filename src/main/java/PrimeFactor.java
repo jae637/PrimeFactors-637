19bd495711d6d88847a9a2c3e09037927b8c0743
cjh <kfc@email.com>
2025-06-04 10:18:34 +0900

    [feat] of Method Green step

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactor {
+    public List<Integer> of(int i) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,11 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
     @Test
     void name(){
         assertEquals(1,1);
+    }
+
+    @Test
+    void primeFactorTest1(){
+        PrimeFactor pf = new PrimeFactor();
+        assertEquals(Arrays.asList(),pf.of(1));
     }
 
 }
