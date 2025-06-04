ac97fb32d05a1e7df109edbe03384e4680d460b7
cjh <kfc@email.com>
2025-06-04 10:40:10 +0900

    [refector] 37p generalizing

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,9 +4,7 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> result = new ArrayList<>();
-        if(number==2){
-            result.add(number);
-        }else if(number ==3){
+        if(number>1){
             result.add(number);
         }
         return result;
